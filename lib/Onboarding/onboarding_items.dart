f8c5ae4db851d55d29f614f95f8828502ae6104c
unknown <iyonissio01@gmail.com>
2024-08-27 22:58:04 +0200

    first commit

---
+++ lib/Onboarding/onboarding_items.dart
@@ -0,0 +1,22 @@
+import 'package:ola_mundo/Onboarding/onboarding_info.dart';
+
+class OnboardingItems {
+  List<OnboardingInfo> items = [
+    OnboardingInfo(
+        title: "Laboratory",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc imperdiet, erat ut lacinia ullamcorper, ex dolor pulvinar enim, in porttitor ligula nunc quis erat. Mauris accumsan auctor quam, at convallis metus sagittis id. ",
+        image: "assets/InstantInformation-cuate.png"),
+    OnboardingInfo(
+        title: "Diagnosis",
+        description: "ed vel lacus posuere dolor tempor blandit. Aenean auctor, elit et semper laoreet, leo ante lacinia massa, et feugiat nunc odio ut orci. Nam euismod ipsum id nibh molestie elementum.",
+        image: "assets/OnlineArticle-cuate.png"),
+    OnboardingInfo(
+        title: "Chronic Disseases",
+        description: "Aliquam ultrices eros ut elementum semper. Donec at pellentesque ligula. In dictum congue sem, eget condimentum leo bibendum vitae. Nulla imperdiet sapien vel ex accumsan ornare. ",
+        image: "assets/InstantInformation-cuate.png"),
+    OnboardingInfo(
+        title: "Disseases",
+        description: "Pellentesque porta consequat nulla, et mollis enim consectetur sit amet. Pellentesque eu turpis placerat, commodo neque quis, convallis ante. ",
+        image: "assets/OnlineArticle-cuate.png"),
+  ];
+}
---
+++ lib/Onboarding/onboarding_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:ola_mundo/Onboarding/onboarding_items.dart';
+
+class OnboardingView extends StatefulWidget {
+  const OnboardingView({super.key});
+
+  @override
+  State<OnboardingView> createState() => _OnboardingViewState();
+}
+
+class _OnboardingViewState extends State<OnboardingView> {
+  final controller = OnboardingItems();
+  final pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 15),
+        child: PageView.builder(
+            itemCount: controller.items.length,
+            controller: pageController,
+            itemBuilder: (context, index) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(controller.items[index].image),
+                  const SizedBox(height: 15),
+                  Text(controller.items[index].title,
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+                  const SizedBox(height: 15),
+                  Text(controller.items[index].description,
+                    style: const TextStyle(color: Colors.grey),textAlign: TextAlign.center,),
+                ],
+              );
+            }),
+      ),
+    );
+  }
+}
